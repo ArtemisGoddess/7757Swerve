f2a9d616b09c8743441a2141b5a4483a995cf40f
ArtemisGoddess <xpyzwilke@gmail.com>
2025-03-30 12:28:48 -0400

    - ACTUALLY removed unnecessary boolean and... - I forgot to update the RobotContainer.

--- src/main/cpp/commands/AlignAndDrive.cpp
+++ src/main/cpp/commands/AlignAndDrive.cpp
@@ -1,14 +1,14 @@
 #include "commands/AlignAndDrive.h"
 
-AlignAndDrive::AlignAndDrive(VisSubsystem* vis, subsystems::CommandSwerveDrivetrain* drivetrain, bool withAprilTag) 
-    : m_vis(vis), m_drivetrain(drivetrain), m_withAprilTag(withAprilTag) {
+AlignAndDrive::AlignAndDrive(VisSubsystem* vis, subsystems::CommandSwerveDrivetrain* drivetrain) 
+    : m_vis(vis), m_drivetrain(drivetrain) {
     AddRequirements({vis, drivetrain});
 }
 
 void AlignAndDrive::Initialize() { }
 
 void AlignAndDrive::Execute() {
-    m_vis->alignAndDrive(m_withAprilTag, m_drivetrain);
+    m_vis->alignAndDrive(m_drivetrain);
 }
 
 void AlignAndDrive::End(bool interrupted) { }
